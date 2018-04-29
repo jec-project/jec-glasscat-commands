97cefb068923c2bd62c7252fadd5e881b7d09be9
Pascal ECHEMANN <developers@flashapi.org>
2018-04-29 16:26:27 +0200

    Minor Update
    Fixing some misspelling. Adding links to the new JEC website.

--- types/jec-glasscat-commands.d.ts
+++ types/jec-glasscat-commands.d.ts
@@ -2,11 +2,38 @@
  * JEC GlassCat Commands Node Module
  * Copyright(c) 2017-2018 Pascal ECHEMANN
  * Apache 2.0 Licensed
- * This is a part of the JEC Projects: <https://github.com/pechemann/JEC>
+ * This is a part of the JEC projects: <http://jecproject.org>
  */
 
 declare module "jec-glasscat-commands" {
 
 import {ScriptCommand, AbstractScriptCommand, CommandDescriptor} from "jec-glasscat-cli";
 
-export class BuildArchetype extends AbstractScriptCommand implements ScriptCommand {    constructor();    private buildProcessor();    private buildRequest(argv);    execute(argv: any, callback: (err: any) => void): void;    getHelp(argv: any): CommandDescriptor;}export class CreateBootstrapFile extends AbstractScriptCommand implements ScriptCommand {    constructor();    private createTemplate(className, glasscatPath);    private createCompiledTemplate(className, glasscatPath);    execute(argv: any, callback: (err: any) => void): void;    getHelp(argv: any): any;}export class CreateJslet extends AbstractScriptCommand implements ScriptCommand {    constructor();    private createTemplate(className, glasscatPath);    private createCompiledTemplate(className, glasscatPath);    execute(argv: any, callback: (err: any) => void): void;    getHelp(argv: any): any;}export class CreateRole extends AbstractScriptCommand implements ScriptCommand {    constructor();    private createTemplate(className, role, glasscatPath);    execute(argv: any, callback: (err: any) => void): void;    getHelp(argv: any): any;}}
+export class BuildArchetype extends AbstractScriptCommand implements ScriptCommand {
+    constructor();
+    private buildProcessor();
+    private buildRequest(argv);
+    execute(argv: any, callback: (err: any) => void): void;
+    getHelp(argv: any): CommandDescriptor;
+}
+export class CreateBootstrapFile extends AbstractScriptCommand implements ScriptCommand {
+    constructor();
+    private createTemplate(className, glasscatPath);
+    private createCompiledTemplate(className, glasscatPath);
+    execute(argv: any, callback: (err: any) => void): void;
+    getHelp(argv: any): any;
+}
+export class CreateJslet extends AbstractScriptCommand implements ScriptCommand {
+    constructor();
+    private createTemplate(className, glasscatPath);
+    private createCompiledTemplate(className, glasscatPath);
+    execute(argv: any, callback: (err: any) => void): void;
+    getHelp(argv: any): any;
+}
+export class CreateRole extends AbstractScriptCommand implements ScriptCommand {
+    constructor();
+    private createTemplate(className, role, glasscatPath);
+    execute(argv: any, callback: (err: any) => void): void;
+    getHelp(argv: any): any;
+}
+}
